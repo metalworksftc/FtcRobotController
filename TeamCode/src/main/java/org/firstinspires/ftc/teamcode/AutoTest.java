895c01510a05ed1cbbf773d0da9613e010073a31
metalworks <github@ftc.rocks>
2022-09-18 14:04:45 -0400

    Made the new project and added the new vision code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -4,25 +4,14 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Autonomous(name = "Auto Test")
+@Autonomous(name = "Autonomous")
 
 public class AutoTest extends LinearOpMode {
     Wheels wheels;
     Intake intake;
-
+    Arm arm;
     @Override
     public void runOpMode() {
-        wheels = new Wheels(hardwareMap, telemetry);
-
-        waitForStart();
-
-        wheels.absoluteTurnPower(-90, wheels.driveSpeed);
-        sleep(500);
-        wheels.absoluteTurnPower(180,wheels.driveSpeed);
-        sleep(500);
-        wheels.absoluteTurnPower(90,wheels.driveSpeed);
-        sleep(500);
-        wheels.absoluteTurnPower(0,wheels.driveSpeed);
-        sleep(500);
+        
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewAxisDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewAxisDrive.java
@@ -4,87 +4,17 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name = "New Axis Drive")
+@TeleOp(name = "TeleOp")
 
 public class NewAxisDrive extends OpMode {
-    Wheels wheels;
-    Intake intake;
-//    private Arm arm;
-
-    DcMotor leftFrontMotor, leftRearMotor, rightFrontMotor, rightRearMotor, armMotor;
-
+   
     @Override
     public void init() {
-        wheels = new Wheels(hardwareMap, telemetry);
-        intake = new Intake(hardwareMap,telemetry);
-//        arm = new Arm(hardwareMap, telemetry);
-        intake.servoDown();
+       
     }
-
-    double flywheelSpeed = 0.5;
-
+    
     @Override
     public void loop() {
-
-
-        if (gamepad2.dpad_down) {
-            telemetry.addLine("D-Pad Down");
-            intake.servoStore();
-        }
-
-        if (gamepad2.dpad_right) {
-            telemetry.addLine("D-Pad Right");
-            intake.servoDown();
-        }
-
-        if (gamepad2.dpad_left) {
-            telemetry.addLine("D-Pad Left");
-            intake.servoLowerCap();
-        }
-
-        if (gamepad2.dpad_up) {
-            telemetry.addLine("D-Pad Up");
-            intake.servoUpperCap();
-        }
-
-        telemetry.update();
-
-
-        if (gamepad2.right_bumper) {
-            intake.flywheelMotor.setPower(gamepad2.left_trigger * - flywheelSpeed);
-        } else {
-            intake.flywheelMotor.setPower(gamepad2.left_trigger * flywheelSpeed);
-        }
-
-        if (intake.flywheelMotor.getPower() >= 0.4) {
-            flywheelSpeed += 0.0024;
-            telemetry.addLine(String.valueOf(flywheelSpeed));
-            telemetry.update();
-        } else {
-            flywheelSpeed = 0.5;
-        }
-
-        if (gamepad1.left_bumper) {
-            wheels.reversePower(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x * 0.5f);
-        }
-
-        else {
-            if (gamepad1.right_bumper) {
-                wheels.driveCartesian(gamepad1.left_stick_x * 0.5, gamepad1.left_stick_y * 0.5, gamepad1.right_stick_x * 0.5);
-            }
-            else {
-                wheels.driveCartesian(gamepad1.left_stick_x * 1, gamepad1.left_stick_y * 1, gamepad1.right_stick_x * 0.5);
-                telemetry.addLine(String.valueOf(wheels.leftFrontMotor.getCurrentPosition()));
-                telemetry.addLine(String.valueOf(wheels.rightFrontMotor.getCurrentPosition()));
-                telemetry.addLine(String.valueOf(wheels.leftBackMotor.getCurrentPosition()));
-                telemetry.addLine(String.valueOf(wheels.rightBackMotor.getCurrentPosition()));
-                telemetry.update();
-            }
-        }
-
-            intake.armMotor(gamepad2.left_stick_y);
-        intake.intakeMotor.setPower(gamepad2.right_stick_y * 0.5);
-
-
+        
     }
 }
